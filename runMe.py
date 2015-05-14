0b03dd3f6d8ecaa935fc2b7714e0e3d7013323b9
Phil Kohn <me@philipkohn.com>
2015-05-14 08:42:42 -0400

    Check for folders
    Check for folders, if they don't exist create them

--- runMe.py
+++ runMe.py
@@ -1,22 +1,23 @@
 import os
 clear = lambda: os.system('clear')
 clear()
-print ('\033[1;47m  Version 0.5  \033[1;m', '\n', '\033[1;46m  Created by Philip Kohn | @pin3c0n3  \033[1;m', '\n')
+print ('\033[1;47m  Version 0.6  \033[1;m', '\n', '\033[1;46m  Created by Philip Kohn | @pin3c0n3  \033[1;m', '\n')
 domain = input("Enter domain: ")
 sub = ['aut1odiscover', 'email', 'ftp', 'imap', 'mail', 'pop', 'pop3', 'remote', 'secure', 'sftp', 'sip', 'smtp', 'vpn', 'www']
-#file = open('targets.txt', 'w')
-targetsName = ('target_' + domain + '.txt')
-resultsName = ('results_' + domain + '.txt')
-cmd = ("dig -f ./target/" + targetsName + " +noall +answer > ./results/" + resultsName)
-file = open("./target/" + targetsName, 'w')
+if not os.path.exists("./targets"):
+    os.makedirs("./targets")
+if not os.path.exists("./results"):
+    os.makedirs("./results")
+targetsName = ('./targets/target_' + domain + '.txt')
+resultsName = ('./results/results_' + domain + '.txt')
+cmd = ("dig -f " + targetsName + " +noall +answer > " + resultsName)
+file = open(targetsName, 'w')
 file.write (domain + '\n')
 for i in range(len(sub)):
    file.write (sub[i] + "." + domain + '\n')
 
 file.close()
 
-#os.system( 'dig -f fileName +noall +answer > results.txt' )
-#os.system( 'cat results.txt' )
 os.system(cmd)
-cmd = ("cat ./results/" + resultsName)
+cmd = ("cat " + resultsName)
 os.system(cmd)
